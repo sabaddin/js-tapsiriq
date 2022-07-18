12db1c394ec37ba5401aee0fc7fd0e1bf1a6584f
sabaddin <rzayevsabaddin@gmail.com>
2022-07-18 19:13:59 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+
+//-------------------Ededin tek ve cüt olduğu----------------
+
+
+// let x = prompt("birinci ededi daxil edin");
+// z=parseFloat(x)
+
+
+// if (z % 2 ==0) {
+//     console.log("eded cütdür");
+// }
+
+// else{
+//     console.log("eded tekdir")
+// }
+
+
+
+
+
+
+//----------------Aylar reqemle girme-------------------
+
+
+// let ay =[
+//     "Yanvar","Fevral","Mart",
+//     "Aprel","May","İyun",
+//     "iyul","Avqust","Sentyabr",
+//     "Noyabr","Oktyabr","Dekabr"
+// ];  
+// let x = prompt("Reqem daxil edin");
+// let y = x-1
+// if(y<12){
+//     console.log(ay[y]);
+// }else{
+//     console.log("Xahiş edirem 12den kiçik reqem girin");
+// }
+
+
+
+//--------------Yaş aralığını tapma ----------------------
+
+// let x =prompt("Yaşınızı daxil edin");
+
+// if(x<=15){
+//     console.log("Yaş aralığında uşaq");
+// }
+// else if(x<=18){
+//     console.log("Yaş aralığında yeniyetme");
+// }
+
+// else if(x<=35){
+//     console.log("Yaş aralığında genc");
+// }
+
+// else if(x<=65){
+//     console.log("Yaş aralığında yetişkin");
+// }
+// else if(x<=65){
+//     console.log("Yaş aralığında Yaşlı");
+// }
+
+
+
+//------------------saniye hesablama --------------------
+
+//  let x = prompt("reqem daxil edin");
+
+//  let gun = Math.floor(x / 86400);
+//  let deqiqe = Math.floor(x / 60);
+//  let saat = Math.floor(x / 3600) ;
+
+// console.log(x + "saniye "+ gun + "gün "+ saat +"saat "+ deqiqe+ "deqiqe" );
+
+
+
+
